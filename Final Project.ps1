cfad0d250685414e63e3c8a4b9bda194e73bdb44
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-06 13:56:17 -0500

    Add menu

---
+++ Final Project.ps1
@@ -0,0 +1,20 @@
+Final Project
+
+function Show-Menu
+{
+    Clear-Host
+    Write-Host "                     Menu of Management                "
+    Write-Host 
+    Write-Host "1: Five processes that are consuming the most CPU."
+    Write-Host
+    Write-Host "2: Filesystems or disks connected to the machine."
+    Write-Host
+    Write-Host "3: Name and size of the largest file stored on a disk or filesystem."
+    Write-Host
+    Write-Host "4: Amount of free memory and amount of swap space in use."
+    Write-Host
+    Write-Host "5: Number of currently active network connections"
+    Write-Host
+    Write-Host "B: Press 'B' to go back."
+    Write-Host
+}
