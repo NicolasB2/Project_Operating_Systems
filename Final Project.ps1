7a4c2a22e49445b9f826b80dbd08e4dcfdf4b60c
david1999erazo <david.erazo2@correo.icesi.edu.co>
2020-05-08 21:39:19 -0500

    Complete option three

--- Final Project.ps1
+++ Final Project.ps1
@@ -29,13 +29,16 @@ function optionOne
 
 function optionTwo
 {
-    Get-Disk | ft -Property FriendlyName
+    Get-WmiObject -Class win32_logicaldisk | ft @{n='Name';e={$_.Name}}, @{n='Free Space (BYTE)';e={$_.FreeSpace}}, @{n='Size (BYTE)';e={$_.Size}}
     Read-Host "Press enter to continue"
 }
 
 function optionThree
 {
-    Write-Output "3"
+    
+    $input =Read-Host "Insert the path of the disk or filesystem"
+
+    Get-ChildItem -Path $input -Recurse | sort -Descending Length | select -First 1 | ft @{n='Name';e={$_.Name}}, @{n='Size (BYTE)';e={$_.Length}}, @{n='Path';e={$_.FullName}}
     Read-Host "Press enter to continue"
 }
 
