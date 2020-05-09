0e3412a1dbc0c66c08c77c737a50b835f22c21c0
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-05-09 00:02:26 -0500

    Add option 5

--- Final Project.ps1
+++ Final Project.ps1
@@ -71,7 +71,7 @@ function optionFour
 
 function optionFive
 {
-    
+    Get-NetTCPConnection -State Established | Measure-Object -Line | ft @{n='Connections';e={$_.Lines}}
     Read-Host "Press enter to continue"
 }
 
