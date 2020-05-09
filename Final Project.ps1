132deeae860e050836888cd74168331630eb5bb4
NicolasB2 <nicolas.biojo@correo.icesi.edu.co>
2020-05-08 23:38:18 -0500

    Add option 4

--- Final Project.ps1
+++ Final Project.ps1
@@ -45,23 +45,33 @@ function optionThree
 function optionFour
 {
 
-    $tmaximo = systeminfo | select-string "Memoria virtual: tamaÂ¤o mÂ ximo:"
-    Write-Output $tmaximo
 
-    $tuso = systeminfo | select-string "Memoria virtual: en uso:"
-    Write-Output $tuso
+    $physical = systeminfo | select-string "Memoria f¡sica disponible:"
+    $physical_ = [double]([double][regex]::Matches($physical, '[\d.]+').Value -replace "\,","")*1000000
+    Write-Host "Physical Memory in use (bytes):  "$physical_
 
-    $fisica = systeminfo | select-string "Memoria fÂ¡sica disponible:"
-    Write-Output $fisica
+    $physical_total = systeminfo | select-string "Cantidad total de memoria f¡sica:"
+    $physical_total_ = [double]([double][regex]::Matches($physical_total, '[\d.]+').Value -replace "\,","")*1000000
+    Write-Host "Physical Memory in use (%):      "(($physical_ / $physical_total_)* 100)" %"
 
-    $memoriaF = systeminfo | select-string "Cantidad total de memoria fÂ¡sica:"
-    Write-Output $memoriaF 
+    
+    $virtual_use = systeminfo | select-string "Memoria virtual: en uso:"
+    $virtual_use_ = [double]([double][regex]::Matches($virtual_use, '[\d.]+').Value -replace "\,","")*1000000
+    Write-Host "swap Memory in use (bytes):      "$virtual_use_
+
+    $virtual_max = systeminfo | select-string "Memoria virtual: tama¤o m ximo:"
+    $virtual_max_ = [double]([double][regex]::Matches($virtual_max, '[\d.]+').Value -replace "\,","")*1000000
+
+    Write-Host "swap Memory in use (%):          "(($virtual_use_ / $virtual_max_)*100)" %"
+
+
+    Read-Host "Press enter to continue"
     
 }
 
 function optionFive
 {
-    Write-Output "5"
+    
     Read-Host "Press enter to continue"
 }
 
